107aace6ea5535ede99aa241e01ed5466fdca97a
yoa <yoa@DESKTOP-422OCVL>
2023-06-30 17:47:52 +0300

    Добавьте файлы проекта.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "window.h"
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
---
+++ src/window.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+/*void showWindow() {
+	RenderWindow window( VideoMode(200, 200), "Test!");
+	CircleShape MyC(100.f);
+	MyC.setFillColor(Color::Green);
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(MyC);
+		window.display();
+	}
+}*/
